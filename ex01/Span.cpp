4df358e83c57f69ebf8dfd2136201bad3fdab559
ichouare <issam.chouaref1998@gmail.com>
2023-09-17 18:51:52 +0100

    here

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -5,22 +5,43 @@ Span::Span()
 
 }
 
-Span:Span(unsigned int n)
+Span::Span(unsigned int N):N(N)
 {
 
 }
 
 
-Span::Span(const Span& original)
-{
-    *this = original;
-}
+// Span::Span(const Span& original)
+// {
+//     *this = original;
+// }
 
-Span& Span::operator=(const Span& original)
-{
-    if(this = &original)
-        return(*this);
+// Span& Span::operator=(const Span& original)
+// {
+//     // if(this = &original)
+//     //     return(*this);
     
+// }
+
+void Span::addNumber(int number)
+{
+ 
+    if(arr.size() < N)
+    {
+        if(arr.size() != 0)
+        {
+            it = arr.find(number);
+            if(it == arr.end())
+                throw -1;
+
+        }
+        std::cout <<": " <<*it << std::endl;
+        arr.insert(number);
+    }
+    std::cout << "myset contains:";
+  for (it=arr.begin(); it!=arr.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
 }
 
 
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,17 +1,22 @@
 #ifndef SPAN_H
 #define SPAN_H
 
+#include <iostream>
+#include  <set>
 
 class Span 
 {
     public :
-        Span(unsigned int n);
-        Span(const Span& original);
-        Span& operator=(const Span& original);
+        Span(unsigned int N);
+        // Span(const Span& original);
+        // Span& operator=(const Span& original);
+        void addNumber(int number);
     private:
         Span();
-        std::vector<int> v
+        std::set<int> arr;
+        unsigned int N;
+        std::set<int>::iterator it;
 
 };
 
-#ifend
+#endif
